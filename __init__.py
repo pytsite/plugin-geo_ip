ee1a594a7e57f2c07f90e0e74992d73826d43fa2
Alexander Shepetko <a@shepetko.com>
2017-12-02 22:37:49 +0200

    Release 1.1

--- __init__.py
+++ __init__.py
@@ -1,22 +1,9 @@
 """PytSite Geo IP
 """
 # Public API
-from . import _model as model, _error as error
+from . import _error as error
 from ._api import resolve
 
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
-
-
-def _init():
-    from pytsite import cron
-    from plugins import odm
-    from . import _eh
-
-    odm.register_model('geo_ip', model.GeoIP)
-
-    cron.weekly(_eh.cron_weekly)
-
-
-_init()
--- _api.py
+++ _api.py
@@ -1,16 +1,16 @@
-"""PytSite Geo IP Functions.
+"""PytSite Geo IP API Functions
 """
 import requests as _requests
 import re as _re
-from plugins import odm as _odm
-from . import _model, _error
+from pytsite import cache as _cache
+from . import _error
 
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
-
 _private_ip_re = _re.compile('(127\.|10\.|172\.(16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32)|192\.168\.)')
+_cache_pool = _cache.create_pool('geo_ip')
 
 # external_api_field: our_field
 _field_mapping = {
@@ -22,29 +22,41 @@ _field_mapping = {
     'lat': 'latitude',
     'lon': 'longitude',
     'org': 'organization',
-    'zip': 'postal_code',
     'region': 'region',
     'regionName': 'region_name',
-    'timezone': 'timezone',
 }
 
 
-def resolve(ip: str) -> _model.GeoIP:
-    """Get data about an IP address.
+def resolve(ip: str) -> dict:
+    """Get data about an IP address
     """
-    # Checking for previously fetched data
-    entity = _odm.find('geo_ip').eq('ip', ip).first()
-    if entity:
-        return entity
+    try:
+        return _cache_pool.get(ip)
 
-    # Fetching data
-    entity = _odm.dispense('geo_ip').f_set('ip', ip)
-    if ip != '0.0.0.0' and not _private_ip_re.match(ip):
-        r = _requests.get('http://ip-api.com/json/{}'.format(ip))
-        if r.status_code != 200:
-            raise _error.ResolveError(r.text)
-        for ext_api_f, val in r.json().items():
+    except _cache.error.KeyNotExist:
+        r = {
+            'asn': 'Unknown',
+            'city': 'Unknown',
+            'country': 'Unknown',
+            'country_code': 'Unknown',
+            'isp': 'Unknown',
+            'latitude': 0.0,
+            'longitude': 0.0,
+            'organization': 'Unknown',
+            'region': 'Unknown',
+            'region_name': 'Unknown',
+        }
+
+        if ip == '0.0.0.0' or _private_ip_re.match(ip):
+            return r
+
+        resp = _requests.get('http://ip-api.com/json/{}'.format(ip))
+
+        if resp.status_code != 200:
+            raise _error.ResolveError(resp.text)
+
+        for ext_api_f, val in resp.json().items():
             if ext_api_f in _field_mapping:
-                entity.f_set(_field_mapping[ext_api_f], val)
+                r[_field_mapping[ext_api_f]] = val
 
-    return entity.save()
+        return _cache_pool.put(ip, r)
