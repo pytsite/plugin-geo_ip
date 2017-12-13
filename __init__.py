f63e3d7d909bdbd66c963f9c4f29080e3005a5b4
Alexander Shepetko <a@shepetko.com>
2017-12-13 17:19:57 +0200

    Release 1.2

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 """PytSite Geo IP
 """
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
 from pytsite import plugman as _plugman
 
 # Public API
 if _plugman.is_installed(__name__):
     from . import _error as error
     from ._api import resolve
-
-__author__ = 'Alexander Shepetko'
-__email__ = 'a@shepetko.com'
-__license__ = 'MIT'
